6651e4422f6472a5f52dbdab85e22a0fb4854eb1
Petru <petrubodea94@gmail.com>
2020-06-17 21:53:38 +0300

    added max parallel requests on the same host functionality

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 When *crawl()* starts executing, it will keep on populating **urls** with new unvisited URLs. The recursion should stop when **urls** is empty (that takes a quite some time...).
 
 If a link is not responding, it will try fetching the resource 3 times until marking the link as visited and moving on.
+
+**MAX_PARALLEL_REQUESTS** (default 20) requests can be made in parallel.
+
+**MAX_HOST_PARALLEL_REQUESTS** (default 4) requests on the same host can be made in parallel
--- web-crawler.js
+++ web-crawler.js
@@ -5,7 +5,6 @@ const MAX_HOST_PARALLEL_REQUESTS = 4;
 const MAX_URL_LENGTH = 2048;
 
 var urls = ["https://iclp.imfast.io/"];
-var urlsToVisit = [];
 var visitedUrls = [];
 
 var options = {
@@ -16,19 +15,17 @@ var options = {
 crawl();
 
 async function crawl(){
-    if(urls.length <= MAX_PARALLEL_REQUESTS){
-        urlsToVisit = urls;
-    }else{
-        urlsToVisit = urls.slice(0, MAX_PARALLEL_REQUESTS);
-    }
+    var urlsToVisit = getUrlsToVisit();
     console.log("\x1b[36mThere are " + urls.length + " URLs left to visit...\x1b[0m");
-    console.log("\x1b[36mTrying to visit " + urlsToVisit.length + " URLs...\x1b[0m");
+    console.log("\x1b[36mTrying to visit " + urlsToVisit.length + " URLs...\x1b[0m \n" + urlsToVisit);
+
     await Promise.all(urlsToVisit.map(async (item)=>{
         try{
             if(!visitedUrls.includes(item)){
-                var domain = getDomain(item);
+                var domain = getHttpDomain(item)[1];
+
                 var content = await getContent(item, PAGE_RETRY_ATTEMPTS);
-                var found = await parseHtml(content, domain[1]);
+                var found = await parseHtml(content, domain);
                 // console.log("On node \x1b[4m\x1b[1m\x1b[32m" + item + "\x1b[0m found: \x1b[32m" + found + "\x1b[0m");
                 updateUrls(item);
             }
@@ -36,14 +33,11 @@ async function crawl(){
             console.warn("\x1b[31mSomething went wrong:\x1b[0m", err);
         }
     }));
-     
+
+    console.log("\x1b[36mVisited a total of " + visitedUrls.length + " URLs.\x1b[0m");
     if(urls.length > 0){
-        console.log("\x1b[36mVisited a total of " + visitedUrls.length + " URLs.\x1b[0m");
         crawl();
-    } else{
-        console.log("\x1b[36mVisited a total of " + visitedUrls.length + " URLs.\x1b[0m");
-    }
-    
+    } 
 }
 
 async function getContent(url, attemptsNr){
@@ -100,7 +94,42 @@ function updateUrls(url){
     urls.splice(urls.indexOf(url), 1);
 }
 
-function getDomain(url){
+function getHttpDomain(url){
     var domaineRE = /^((?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?[^:\/?\n]+)/g;
     return domaineRE.exec(url);
+}
+
+function getDomain(url){
+    var domaineRE = /^(?:https?:\/\/)?((?:[^@\/\n]+@)?(?:www\.)?[^:\/?\n]+)/g;
+    return domaineRE.exec(url);
+}
+
+function getUrlsToVisit(){
+    var urlsToVisit = [];
+    var domainOccurenceMap = new Map();
+
+    for(var i=0; i < urls.length; i++){
+        if(urlsToVisit.length < MAX_PARALLEL_REQUESTS){
+            var domain = getDomain(urls[i])[1];
+            var occurenceNr = domainOccurenceMap.get(domain);
+
+            if(occurenceNr !== undefined){
+                if(occurenceNr < MAX_HOST_PARALLEL_REQUESTS){
+                    urlsToVisit.push(urls[i]);
+                }else{
+                    // console.log("-----------------------> host: " + domain + "; occurences: " + occurenceNr);
+                }
+
+                domainOccurenceMap.set(domain, ++occurenceNr);
+            }else{
+                occurenceNr = 1;
+                domainOccurenceMap.set(domain, occurenceNr);
+
+                urlsToVisit.push(urls[i]);
+            }
+        }else{
+            break;
+        }
+    }
+    return urlsToVisit;
 }
